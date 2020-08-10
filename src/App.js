897da61a0c09a24e23ad78cbfd1b302de655897e
rid3thespiral <davidebattaglia1993@gmail.com>
2020-08-10 19:17:15 +0200

    list movies

--- src/App.js
+++ src/App.js
@@ -3,15 +3,37 @@ import './App.css';
 const APIKEY = '1328aa12';
 const API_URL = 'http://www.omdbapi.com';
 
+function fetchMovies(search = '=back to the future'){
+  return fetch(API_URL+ '?apikey=' + APIKEY+'&s='+search).then(res => res.json());
+};
+
 class App extends Component{
-  componentDidMount(){
-    fetch(API_URL+ '?apikey=' + APIKEY+'&s=back to the future').then(res => res.json())
-    .then(apiResult => console.log(apiResult));
+
+  constructor(props){
+    super(props);
+    this.state = {
+      movies : [],
+      totalCount: 0
+    }
   }
+
+  componentDidMount(){
+    fetchMovies().then(res => {
+      this.setState({
+        movies: res.Search
+      })
+    })
+  }
+
   render(){
   return (
     <div className="App">
       <h1>My favourite movies</h1>
+      <ul>
+      {
+        this.state.movies.map(movie => <li key={movie.imdbID}>{movie.Title}</li>)
+      }
+      </ul>
     </div>
   );
   }
